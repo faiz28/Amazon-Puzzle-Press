167f10ad8833fbdd4dc9a4069133f8b548de8c7a
Faiz Ahmed <faizcse28@gmail.com>
2021-10-11 01:56:52 +0600

    gitignore create

--- puzzle/puzzle/settings.py
+++ puzzle/puzzle/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'homepage.apps.HomepageConfig',
+    'upload.apps.UploadConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- puzzle/upload/models.py
+++ puzzle/upload/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    file_upload = models.FileField(upload_to='documents/%Y/%m/%d')
