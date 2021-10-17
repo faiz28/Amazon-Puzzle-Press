18d93d01fadaad3cb57d10e4df696f526f766d8e
Faiz Ahmed <faizcse28@gmail.com>
2021-10-17 12:36:21 +0600

    update file create option

--- puzzle/upload/views.py
+++ puzzle/upload/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import redirect, render
 from .forms import Up_fileForm
 from django.utils.datastructures import MultiValueDictKeyError
 from .models import Up_file
-import PyPDF2
-import os
+import PyPDF2,random,os,glob
 
 
 def make_file(str_p):
@@ -17,50 +16,139 @@ def make_file(str_p):
     print(dirspot)
     
 
+    # f = open('./media/file/file1.txt', 'w')
+    # wordlist = {}
+    # cnt = 0
+    # pdf_num = 1
+    # for i in range(3,pages):
+    # # Creating a page object
+    #         pageObj = pdfReader.getPage(i)
+    #         # print("page number ",i)
+    #         # Extracting text from page
+    #         # And splitting it into chunks of lines
+    #         text = pageObj.extractText().split("  ")
+
+    #         # file open 
+            
+    #         for i in range(len(text)):
+    #                 word=""
+    #                 for single_char in text[i]:
+    #                     #make upper case
+    #                     if ((single_char>='a' and single_char <='z') or (single_char>='A' and single_char<='Z')):
+    #                             word += single_char.upper() 
+    #                     else: 
+    #                         wordlist[word]=1
+    #                         if wordlist[word]==2: 
+    #                             continue
+    #                         else: 
+    #                             wordlist[word] = 2
+    #                             if(len(word) >= 6 and len(word)<=12):
+    #                                 f.write(word)
+    #                                 f.write('\n')
+    #                                 # print(word,end="\n")
+    #                                 cnt+=1
+    #                                 if(cnt == 26):
+    #                                     cnt = 0
+    #                                     f.close()
+    #                                     pdf_num += 1
+    #                                     if pdf_num>500:
+    #                                         break
+    #                                     f = open('./media/file/file'+str(pdf_num)+'.txt', 'w')
+    #                         word=""
+    # pdfFileObj.close()
+    # f.close()
+
+
     f = open('./media/file/file1.txt', 'w')
     wordlist = {}
     cnt = 0
     pdf_num = 1
-    for i in range(26,pages):
-    # Creating a page object
+    value = []
+    file_on_off=1
+    for i in range(1,3):
+            # Creating a page object
             pageObj = pdfReader.getPage(i)
             print("page number ",i)
-            # Extracting text from page
-            # And splitting it into chunks of lines
-            text = pageObj.extractText().split("  ")
-
-            # file open 
+            text = pageObj.extractText().split(" ")
+            print(text)
+        
             
             for i in range(len(text)):
-                    word=""
-                    for single_char in text[i]:
-                        #make upper case
-                        if ((single_char>='a' and single_char <='z') or (single_char>='A' and single_char<='Z')):
-                                word += single_char.upper() 
-                        else: 
-                            wordlist[word]=1
-                            if wordlist[word]==2: 
-                                continue
-                            else: 
-                                wordlist[word] = 2
-                                if(len(word) >= 6 and len(word)<=12):
-                                    f.write(word)
-                                    f.write('\n')
-                                    # print(word,end="\n")
-                                    cnt+=1
-                                    if(cnt == 26):
-                                        cnt = 0
-                                        f.close()
-                                        pdf_num += 1
-                                        if pdf_num>500:
-                                            break
-                                        f = open('./media/file/file'+str(pdf_num)+'.txt', 'w')
-                            word=""
                     
-            # For Seprating the Pages
-            print()
-    # closing the pdf file object
-    pdfFileObj.close()
+                    word=""
+                    upper_case = text[i].upper()
+                    # if str("\n") in upper_case:
+                    #     print(upper_case)
+                    # upper_case = ''.join(char for char in upper_case if char.isalnum())
+                    # print(upper_case)
+
+                    # 
+
+                    
+                    check = 0
+                    local_word = ''
+                    for char in upper_case:
+                        if char.isalpha():
+                            check = 1
+                            local_word += char
+                        else:
+                            # print("char = %s ,index = %s"%(char,upper_case.index(char)))
+                            if check==1 or (char=='\n' and upper_case.index(char)!=0) :
+                            
+                                # print("dfdfdfdfd %s"%local_word)
+                                upper_case = local_word
+                                break
+                    
+
+                    upper_case=upper_case.replace('\n','')
+                    if upper_case.isalpha():
+                        print(upper_case)
+                        if len(upper_case) >= 3 and  len(upper_case) <= 12:
+                            if wordlist.get(upper_case)==None:
+                                # print(upper_case)
+                                f.write(upper_case)
+                                f.write('\n')
+
+                                value.append(upper_case)
+                                wordlist[upper_case] = 1
+                                cnt+=1
+
+                                if(cnt == 26):
+                                    cnt = 0
+                                    f.close()
+                                    pdf_num += 1
+                                    if pdf_num>500:
+                                        file_on_off = 0
+                                        break
+                                    else:
+                                        file_on_off = 1
+                                        f = open('./media/file/file'+str(pdf_num)+'.txt', 'w')
+
+
+
+    if pdf_num<500: 
+        pdf_num -=1
+        f.close()
+        f = open('./media/file/file'+str(pdf_num)+'.txt', 'w')
+
+    cnt = 0 
+    while(pdf_num<=500):
+        ran = random.randint(1,len(value)-1)
+        print(ran)
+        f.write(value[ran])
+        f.write('\n')
+        cnt+=1
+
+        if cnt==26:
+            cnt = 0
+            f.close()
+            pdf_num += 1
+            if pdf_num<=500: 
+                f = open('./media/file/file'+str(pdf_num)+'.txt', 'w')
+def  clear_file(str_p):
+    # str_p = str_p+"/*.txt"
+    for file in glob.glob(str_p):
+        print(file)
 
 # Create your views here.
 def upload(request):
@@ -82,7 +170,9 @@ def upload(request):
             #     print(i.file_upload)
             str_p = str(pdf_file.file_upload)
             str_p = "./media/"+str_p
+            # clear_file(str_p)
             make_file(str_p)
+
 
             
 
